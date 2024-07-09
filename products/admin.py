abd11db7e74aba310e6bf30ce7b9a052b4ca3dea
CharithaGundala <charithagundala2004@gmail.com>
2024-07-09 08:15:09 +0530

    admin site registeration ,model inclusion

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(FoodItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Transaction)
+admin.site.register(Delivery)
+admin.site.register(Reward)
+admin.site.register(UserReward)
+admin.site.register(Coin)
+admin.site.register(Competition)
+admin.site.register(Seller)
+admin.site.register(ShippingInformation)
