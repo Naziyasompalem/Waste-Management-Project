d128f639e61b12e38aea959181418ebb5e8cb65f
CharithaGundala <charithagundala2004@gmail.com>
2024-07-19 18:37:03 +0530

    added Email processors

--- products/templatetags/custom_filters.py
+++ products/templatetags/custom_filters.py
@@ -2,6 +2,16 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+register = template.Library()
+
+@register.filter(name='mul')
+def multiply(value, arg):
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return ''
