6dbaa67fca201e9c487d83197a34f85d8991834b
CharithaGundala <charithagundala2004@gmail.com>
2024-07-19 21:32:28 +0530

    User Profile Logging

--- products/context_processors.py
+++ products/context_processors.py
@@ -2,7 +2,3 @@ def user_email(request):
     if request.user.is_authenticated:
         return {'user_email': request.user.email}
     return {user_email: 'Please login into your account'}
-
-def is_user(request):
-    print(str(request.user))
-    return {'user': None if str(request.user)=="AnonymousUser" else request.user}
